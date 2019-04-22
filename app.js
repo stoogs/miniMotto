58b4e5ed448546f23c1dd5d740f59110ae371cd8
stoo <stoo@stoos-iMac.local>
2019-04-22 15:47:04 +0100

    Working bcrypt and salting

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const app = express();
 
 app.use(express.static("public"));
@@ -13,10 +15,10 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/mmUserDB",{useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+})
 
 const User = new mongoose.model("User", userSchema);
 
@@ -32,22 +34,39 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req,res){
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    });
 
+    bcrypt.hash(req.body.password, saltRounds, function(err,hash){   
+        const newUser = new User({
+        email: req.body.username,
+        password: hash
+        })
     newUser.save(function(err){
         if (err) {
-            console.log("HITMIT")
             console.log(err);
         } else {
             res.render("registered")
         }
     });
+})
 });
+app.post("/login", function(req,res){
+    const username = req.body.username;
+    constpassword = req.body.password;
 
-
+    User.findOne({email: username}, function(err,foundUser){
+        if (err) { 
+        console.log(err);
+        } else {
+        if (foundUser) {
+            bcrypt.compare(password, foundUser.password, function(err,result) {
+                if (result === true){
+                    res.render("registered")
+                }
+            });
+        }
+    }
+    })
+});
 
 app.listen(3000, function() {
     console.log("Server started on Port 3000")
