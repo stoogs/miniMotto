fcbacedfc42dfea1f16a0b082e98d036b307afbc
stoo <stoo@stoos-iMac.local>
2019-05-10 10:29:10 +0100

    Added dates to User Schema for posts and user Creation

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ mongoose.set("useCreateIndex", true);
 const userSchema = new mongoose.Schema ({
   email: String,
   password: String,
-  motto : [ {category: String, motto: String} ]
+  motto : [ {category: String, motto: String,
+    mottoDate: { type: Date, default: Date.now } } ],
+  userCreatedDate: { type: Date, default: Date.now } 
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -61,7 +63,6 @@ app.get("/register", function(req, res){
 
 app.get("/userHomepage", function(req, res){
   if (req.isAuthenticated()){
-    // console.log(req)
     res.render("userHomepage", {userData: req.user});
   } else {
     res.redirect("/login");
@@ -78,13 +79,14 @@ app.get("/submit", function(req,res){
 
 app.post("/submit", function(req,res){
     const submittedMotto = req.body.addAMotto;
-    
+    const categoryMotto = req.body.categoryMotto;
+
     User.findById(req.user.id, function(err, foundUser){
         if (err) {
             console.log(err);
           } else {
             if (foundUser) {
-                foundUser.motto.push( {category: "Category",motto: submittedMotto} );
+                foundUser.motto.push( {category: categoryMotto,motto: submittedMotto} );
                 foundUser.save(function(){
                     res.redirect("/userHomepage")
                 })
@@ -113,8 +115,51 @@ app.post("/register", function(req, res){
 
 });
 
-app.post("/login", function(req, res){
+app.get('/test', function(req,res){
+    
+    User.find(req.body.showAllUsersButton, function(err, foundUsers){
+        if (err) {
+            console.log(err);
+          } else {
+            if (foundUsers) {
+                console.log(foundUsers)
+                }
+            }
+    res.render("test", { userData: foundUsers })
+    });
+});
 
+app.delete('/test', function(req,res){
+    console.log("DELETE");
+    console.log(req,res)
+    res.redirect("/test");
+})
+app.post('/test', function(req,res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+      });
+
+      User.register({username: req.body.username}, req.body.password, function(err, user){
+        if (err) {
+          console.log(err);
+          res.redirect("/register");
+        } else {
+          passport.authenticate("local")(req, res, function(){
+              console.log("user crested", user.username)
+            res.render("test");
+          });
+        }
+      });
+
+    //   let name = User.find ({ username: 'qwe' },function (err, name){
+    //       console.log(name);
+    //   });
+      
+      
+})
+
+app.post("/login", function(req, res){
   const user = new User({
     username: req.body.username,
     password: req.body.password
@@ -132,11 +177,6 @@ app.post("/login", function(req, res){
   });
 
 });
-
-
-
-
-
 
 
 app.listen(3000, function() {
